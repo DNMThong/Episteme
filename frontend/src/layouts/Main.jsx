68e649f3a271abc300ddede59925cae5d092215b
Whisper <nghia14802@gmail.com>
2023-08-14 11:30:22 +0700

    update About Page

--- frontend/src/layouts/Main.jsx
+++ frontend/src/layouts/Main.jsx
@@ -2,28 +2,29 @@ import { useEffect } from "react";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 
 const Main = () => {
-  useEffect(() => {
-    document.title = "Trang chủ";
-  }, []);
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "100vh",
-      }}>
-      <Navbar />
-      <Box>
-        <Outlet />
+   useEffect(() => {
+      document.title = "Trang chủ";
+   }, []);
+   return (
+      <Box
+         sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100vh",
+         }}
+      >
+         <Navbar />
+         <Box>
+            <Outlet />
+         </Box>
+         <Paper mt="auto" sx={{}}>
+            <Footer />
+         </Paper>
       </Box>
-      <Box mt="auto">
-        <Footer />
-      </Box>
-    </Box>
-  );
+   );
 };
 
 export default Main;
